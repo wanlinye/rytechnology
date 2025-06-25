237d23a5f39fbde7bc31e3b8f9fac479cfff0e5f
Robert Ye <>
2025-06-25 15:29:55 +1000

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,18 @@ definePageMeta({
 });
 
 var gtmTag = `
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y9BM7XHFYW">
-<\/script>
-<script>
-  window.dataLayer = window.dataLayer || [];
+window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-Y9BM7XHFYW');
-<\/script>
 `;
+useHead({
+  script: {
+    src: "https://www.googletagmanager.com/gtag/js?id=G-Y9BM7XHFYW",
+    async: true,
+  },
+});
 useHead({ script: gtmTag });
 </script>
 
